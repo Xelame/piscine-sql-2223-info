6ed52beacd2f1a9954d18e0c5c07015004b7aeb3
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-02 16:24:13 +0100

    Ex 9

---
+++ double-join.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT playlists.Name AS PlaylistName, tracks.Name AS TrackName
+FROM playlists
+INNER JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
+INNER JOIN tracks ON playlist_track.TrackId = tracks.TrackId
+WHERE playlists.Name = 'TV Shows'
