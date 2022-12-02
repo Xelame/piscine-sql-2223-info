e5936e3052423ed6c9259e5a680ce39584faab8f
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-02 15:48:33 +0100

    Ex 7

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,1 +1,1 @@
-SELECT FirstName, LastName, Company, Total AS InvoiceTotalPrice FROM customers INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
+SELECT FirstName, LastName, Company, Total AS InvoiceTotalPrice FROM customers INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId WHERE Company IS NOT NULL;
