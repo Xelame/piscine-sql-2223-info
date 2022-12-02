30f94d99fb44b4a306f32e1900e0ea3766e3eee4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-02 16:25:41 +0100

    Ex 9

--- double-join.sql
+++ double-join.sql
@@ -3,3 +3,4 @@ FROM playlists
 INNER JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
 INNER JOIN tracks ON playlist_track.TrackId = tracks.TrackId
 WHERE playlists.Name = 'TV Shows'
+LIMIT 100;
