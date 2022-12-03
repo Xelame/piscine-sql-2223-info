4d417c7872bedc87943e040b550a6b4a0369b131
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-03 13:51:38 +0100

    Ex 6

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -1,1 +1,1 @@
-SELECT InvoiceId, Name AS InvoiceItem, UnitPrice FROM Invoice_items INNER JOIN Tracks ON Invoice_items.TrackId = Tracks.TrackId  WHERE InvoiceId = 10 ORDER BY InvoiceItem ASC;
+SELECT InvoiceId, Name AS InvoiceItem, Tracks.UnitPrice FROM Invoice_items INNER JOIN Tracks ON Invoice_items.TrackId = Tracks.TrackId  WHERE InvoiceId = 10 ORDER BY InvoiceItem ASC;
