463e2eab8440ab1c55c4b14b78623e6475d2cabd
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-03 15:00:43 +0100

    Fuck logic

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,7 +1,7 @@
 SELECT invoiceId, Total,
 CASE
-    WHEN Total > 5 THEN 'Price lower than 5$'
-    WHEN Total > 10 THEN 'Price lower than 10$'
+    WHEN Total < 5 THEN 'Price lower than 5$'
+    WHEN Total < 10 THEN 'Price lower than 10$'
     ELSE 'Price higher than 10$'
 END AS 'CASE'
 FROM invoices
