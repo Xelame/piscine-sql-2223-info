210eeedc198439ab5638fde43d76fab78f9c649e
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-03 17:29:54 +0100

    Ex 6

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -4,7 +4,6 @@ CASE
     WHEN 1 < COUNT(*) AND COUNT(*) < 10 THEN 'Productive'
     WHEN 10 <= COUNT(*) THEN 'Very Productive'
 END AS IsProductive
-FROM artists 
+FROM (SELECT * FROM artists LIMIT 100) AS artists
 INNER JOIN albums ON artists.ArtistId = albums.ArtistId
-GROUP BY artists.Name
-LIMIT 100;
+GROUP BY artists.Name;
