8707ba2351b0d8314bbe0db17eb8bd048b7660fd
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-12-06 11:50:11 +0100

    nice tri

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -1,9 +1,10 @@
-SELECT Name, COUNT(Title) AS NbAlbums,
+SELECT Name, COUNT(*) AS NbAlbums,
 CASE
-    WHEN COUNT(Title) = 1 THEN 'Unproductive'
-    WHEN 1 < COUNT(Title) AND COUNT(Title) < 10 THEN 'Productive'
-    WHEN 10 <= COUNT(Title) THEN 'Very Productive'
+    WHEN COUNT(*) = 1 THEN 'Unproductive'
+    WHEN 1 < COUNT(*) AND COUNT(*) < 10 THEN 'Productive'
+    WHEN 10 <= COUNT(*) THEN 'Very Productive'
 END AS IsProductive
 FROM (SELECT * FROM artists LIMIT 100) artists 
 INNER JOIN albums ON artists.ArtistId = albums.ArtistId
-GROUP BY Name WITH ROLLUP;
+GROUP BY Name
+ORDER BY artists.ArtistId;
